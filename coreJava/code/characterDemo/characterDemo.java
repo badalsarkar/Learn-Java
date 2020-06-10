255c716de7eacfcd116c7c9cc0979ec23fc4cc13
badal sarkar <badalsarkar86@gmail.com>
2020-06-10 15:40:49 -0400

    updated note

---
+++ coreJava/code/characterDemo/characterDemo.java
@@ -0,0 +1,13 @@
+//this code prints individual character using a char variable
+//
+//
+
+class characterDemo{
+    public static void main(String args[]){
+        char ch='a';
+        for(int i=0; i<1000; i++){
+            System.out.println("the value of char is : " +ch);
+            ch++;
+        }
+    }
+}
